b375b285d768815a72c983fb857fdefc55873b85
Mayankho <yankhot96@gmail.com>
2019-04-22 23:07:24 -0400

    Changes to app routing

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>My react app</h1>
-      
-      </div>
+      <h1>My react app!</h1>
+      <Person />
+         </div>
     );
-  }
+
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'Hi , im a react app!!!'))
+   }
 }
 
 
