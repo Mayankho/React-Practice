b5768de802d7f30638e7130b8c17ad237322439a
Mayankho <yankhot96@gmail.com>
2019-04-30 22:14:27 -0400

    Update before another eject

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id
-    });
+    }); //Find index function?
 
     const person = {...this.state.persons[personIndex]};
 
