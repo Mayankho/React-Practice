085943eeb9e50bd5bb87de78d426c21a7787f62f
Mayankho <yankhot96@gmail.com>
2019-05-12 18:47:22 -0400

    Changed whole setup to be leaner

--- burger-builder/src/ErrorBoundary/ErrorBoundary.js
+++ burger-builder/src/ErrorBoundary/ErrorBoundary.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-
+//This whole component is a higher order componont, 
+// this will wrap around your component to check ifthere is a error 
 class ErrorBoundary extends React.Component{
     state = {
         hasError: false,
         errorMessage: ''
     }
 
+// This is sa method that recieves the error 
     componentDidCatch = (error, info) => {
         this.setState({hasError:true, errorMessage: error});
     }
+    // This will only react when y=the component you wrap throws an error  
 
     render(){
         if(this.state.hhasError){
@@ -17,6 +20,9 @@ class ErrorBoundary extends React.Component{
         } else {
             return this.props.children
         }
+        // When your accessing props inside a class component use {this}
+        //This component, will only render if there is an error 
+        // This will have a, 
     }
 }
 
