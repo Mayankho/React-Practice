7fc0c1abe2790483d279ff6e6b9b3c2baa00b6f4
Mayankho <yankhot96@gmail.com>
2019-06-04 23:27:29 -0400

    Changed higher order component syntax, made it so props is read

--- burger-builder/src/components/hoc/WithClass.js
+++ burger-builder/src/components/hoc/WithClass.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+// const withClass = (WrappedComponent, className) => {
+//     return (props) => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     )
+// }
+
 const withClass = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent />
-        </div>
-    )
+    return class extends React.Component{
+        render(){
+            return (
+            <div className={className}>
+                <WrappedComponent {...this.props} />
+            </div>
+            )
+        }
+    }   
 }
 
 export default withClass;
